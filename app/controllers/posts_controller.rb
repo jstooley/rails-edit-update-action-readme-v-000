9f1d7ba141819681274cc58ecf983d02cd871df2
Justin Tooley <jstooley823@gmail.com>
2017-12-14 22:48:37 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,16 +7,17 @@ class PostsController < ApplicationController
 		@post = Post.new
 	end
 
-	def show
-		@post = Post.find(params[:id])
-	end
-
 	def create
 	  @post = Post.new
 	  @post.title = params[:title]
 	  @post.description = params[:description]
 	  @post.save
 	  redirect_to post_path(@post)
+	end
+
+
+	def show
+		@post = Post.find(params[:id])
 	end
 
 	def edit
